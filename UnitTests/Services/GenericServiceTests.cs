66aab3345297d95e85bc0a8a37c324568a33c9a8
Urctos <jan.piaskowskii@gmail.com>
2024-09-29 00:09:47 +0200

    LarixApp 0.1.5 (Add tests for the UpdateAsync and DeleteAsync methods )

--- UnitTests/Services/GenericServiceTests.cs
+++ UnitTests/Services/GenericServiceTests.cs
@@ -2,13 +2,6 @@
 using Domain.Entities;
 using Domain.Interfaces;
 using Moq;
-using Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
 using AutoMapper;
 using Microsoft.Extensions.Logging;
 using Application.Dto.DoorsDtos;
@@ -37,7 +30,11 @@ namespace UnitTests.Services
 
             };
 
-            mapperMock.Setup(x => x.Map<Door>(doorDto)).Returns(new Door() { Name = doorDto.Name, Description = doorDto.Description });
+            mapperMock.Setup(x => x.Map<Door>(doorDto)).Returns(new Door()
+            { 
+                Name = doorDto.Name, 
+                Description = doorDto.Description 
+            });
 
             //Act
 
@@ -91,6 +88,63 @@ namespace UnitTests.Services
             doorDto.Name.Should().BeEquivalentTo(door.Name);
             doorDto.Description.Should().NotBeNull();
             doorDto.Description.Should().BeEquivalentTo(door.Description);
+        }
+
+        [Fact]
+        public async Task update_item_async_should_invoke_update_async_on_item_repository()
+        {
+            var  repositoryMock = new Mock<IRepository<Door>>();
+            var mapperMock = new Mock<IMapper>();
+            var loggerMock = new Mock<ILogger<GenericService<Door, DoorDto, CreateDoorDto, UpdateDoorDto>>>();
+
+            var genericService = new GenericService<Door, DoorDto, CreateDoorDto, UpdateDoorDto>(
+                repositoryMock.Object, mapperMock.Object, loggerMock.Object);
+
+            var updateDoorDto = new UpdateDoorDto()
+            {
+                Name = "Updated Test 1",
+                Description = "Updated Opis testowy"
+            };
+
+            var door = new Door(1, "Test name", "test description", 10, 10, 1500, 1, 1, 1, 1);
+
+            mapperMock.Setup(x => x.Map<Door>(updateDoorDto)).Returns(new Door()
+            {
+                Name = updateDoorDto.Name,
+                Description = updateDoorDto.Description,
+                Id = door.Id
+            });
+
+            repositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(door);
+
+            //Act
+            await genericService.UpdateAsync(updateDoorDto);
+
+            //Assert
+            repositoryMock.Verify(x => x.UpdateAsync(It.IsAny<Door>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task delete_item_async_should_invoke_delete_async_on_item_repository()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IRepository<Door>>();
+            var mapperMock = new Mock<IMapper>();
+            var loggerMock = new Mock<ILogger<GenericService<Door, DoorDto, CreateDoorDto, UpdateDoorDto>>>();
+
+            var genericService = new GenericService<Door, DoorDto, CreateDoorDto, UpdateDoorDto>(
+                repositoryMock.Object, mapperMock.Object, loggerMock.Object);
+
+            var door = new Door(1, "Test name", "test description", 10, 10, 1500, 1, 1, 1, 1);
+
+            repositoryMock.Setup(x => x.GetByIdAsync(door.Id)).ReturnsAsync(door);
+            repositoryMock.Setup(x=>x.DeleteAsync(door)).Returns(Task.CompletedTask);
+
+            // Act
+            await genericService.DeleteAsync(door.Id);
+
+            // Assert
+            repositoryMock.Verify(x => x.DeleteAsync(It.IsAny<Door>()), Times.Once);
         }
     }
 }
